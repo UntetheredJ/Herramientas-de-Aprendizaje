aa276a04ad8926669a2667aea426d64f3b5ce332
DanielXMPB <danielxmpb@gmail.com>
2023-10-02 10:10:18 -0500

    Ejemplo JSP

--- Ejercicios de ejemplo/EjemploConexionBaseDeDatos/src/Persistencia/ConexionBD.java
+++ Ejercicios de ejemplo/EjemploConexionBaseDeDatos/src/Persistencia/ConexionBD.java
@@ -35,7 +35,7 @@ public class ConexionBD {
         db = "ejemploDB";
         url = "jdbc:mysql://" + host + "/" + db+"?serverTimezone=UTC"; 		//URL DB
         username = "root";                      			//usuario base de datos global 
-        password = "daniel101002";
+        password = "contraseña";
         try {
             //Asignacin del Driver
             Class.forName(DB_driver);
